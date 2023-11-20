a6f4b79fcfd9182e8132140caad6ca4185f70279
sweisser <weisser.sarah114@gmail.com>
2023-11-20 00:41:16 -0500

    removed unused imports

--- src/main/java/com/demo/employee/employee/advice/AppControllerAdvice.java
+++ src/main/java/com/demo/employee/employee/advice/AppControllerAdvice.java
@@ -3,7 +3,6 @@ package com.demo.employee.employee.advice;
 import com.demo.employee.employee.exceptions.EmailNotUniqueException;
 import com.demo.employee.employee.exceptions.EmployeeNotFoundException;
 import com.demo.employee.employee.models.EmployeeResponse;
-import com.demo.employee.employee.utils.ValidationMessages;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,14 +12,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class AppControllerAdvice {
     @ExceptionHandler(EmployeeNotFoundException.class)
     public ResponseEntity<EmployeeResponse> handleEmployeeNotFoundExceptions(Exception e) {
-        EmployeeResponse response = new EmployeeResponse(null, e.getMessage());
-        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        EmployeeResponse errorResponse = new EmployeeResponse(null, e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(EmailNotUniqueException.class)
     public ResponseEntity<EmployeeResponse> handleNonUniqueEmailExceptions(Exception e) {
-        System.out.println("HEY");
-        EmployeeResponse response = new EmployeeResponse(null, ValidationMessages.EMAIL_NOT_UNIQUE);
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        EmployeeResponse errorResponse = new EmployeeResponse(null, e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
--- src/test/java/com/demo/employee/employee/advice/AppControllerAdviceTest.java
+++ src/test/java/com/demo/employee/employee/advice/AppControllerAdviceTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 
 @SpringBootTest
